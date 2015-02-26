c375f3efe4bc784e93a8f958596f37b67498443b
Phan Le <phan@zenpayroll.com>
2015-02-26 07:10:33 +0700

    Revert "Always using TLSv1 to talk to plaid when doing auth" This did not help with the intermittent failures
    This reverts commit fc73f86f03fa60afc887db6cf4ebddc1dcb278dc.

--- lib/plaid/auth.rb
+++ lib/plaid/auth.rb
@@ -1,7 +1,6 @@
 module Plaid
   class Auth
     class << self
-      SSL_VERSION = 'TLSv1'
       def add(type, credentials, options = nil)
         parse_response(post('auth',
           type: type,
@@ -25,16 +24,10 @@ module Plaid
 
       def post(path, params)
         url = Plaid.base_url + path
-        request = RestClient::Request.new(
-          url: url,
-          method: :post,
-          payload: {
-            client_id: Plaid.client_id,
-            secret: Plaid.secret
-          }.merge!(params).to_json,
-          headers: {content_type: :json},
-          ssl_version: SSL_VERSION)
-        request.execute { |rsp, _, _| rsp }
+        RestClient.post(url, {
+          client_id: Plaid.client_id,
+          secret: Plaid.secret
+        }.merge!(params).to_json, content_type: :json) { |rsp, _, _| rsp }
       end
 
       def parse_response(response)
