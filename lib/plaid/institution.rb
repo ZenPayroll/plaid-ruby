e7f56e295550a1b10987ef3dda8eb59a0e6f904d
Achint Goel <achintg12@gmail.com>
2017-01-24 14:33:00 -0800

    updating institutions for plaid

--- lib/plaid/institution.rb
+++ lib/plaid/institution.rb
@@ -1,14 +1,28 @@
 module Plaid
   class Institution
     class << self
-      def all
-        response = RestClient.get('institutions')
-        { code: response.code, institutions: response.body }
+      def all(count=50, offset=0)
+        response = RestClient.post('institutions/all',
+          count: count,
+          offset: offset
+        )
+
+        parse_response(response)
       end
 
       def search(query)
-        response = RestClient.get("institutions/search?q=#{CGI.escape(query)}")
+        response = RestClient.get("institutions/all/search?q=#{CGI.escape(query)}")
         { code: response.code, suggestions: response.body }
+      end
+
+      def parse_response(response)
+        parsed = response.body
+        case response.code
+        when 200
+          { code: response.code, total: parsed['total_count'], institutions: parsed['results']}
+        else
+          { code: response.code, error: parsed }
+        end
       end
     end
   end
